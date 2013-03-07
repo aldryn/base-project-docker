e13c29fce3944b34c9ddc981fb730564cfd04033
Jonas Obrist <ojiidotch@gmail.com>
2013-03-08 01:30:16 +0900

    forgot to add urls

--- cmscloud/urls.py
+++ cmscloud/urls.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from cmscloud.views import Add, Delete
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns('',
     url(r'^check-plugins/$', 'cmscloud.views.check_plugins'),
     url(r'^check-apphooks/$', 'cmscloud.views.check_apphooks'),
+    url(r'^add-file/$', csrf_exempt(Add.as_view())),
+    url(r'^delete-file/$', csrf_exempt(Delete.as_view())),
 )
