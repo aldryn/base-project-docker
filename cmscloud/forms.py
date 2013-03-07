44030686b471f558146f6a9b687c7055c17a9fb2
Jonas Obrist <ojiidotch@gmail.com>
2013-03-08 01:16:05 +0900

    sync api

---
+++ cmscloud/forms.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import hmac
+from django import forms
+from django.conf import settings
+from django.utils.crypto import constant_time_compare
+
+
+class DeleteForm(forms.Form):
+    signature = forms.CharField(required=True)
+    path = forms.CharField(required=True)
+
+    def clean_path(self):
+        path = self.cleaned_data.get('path', '')
+        if not path.startswith(('static/', 'templates/')):
+            raise forms.ValidationError("Invalid path")
+        return path
+
+    def clean(self):
+        data = super(DeleteForm, self).clean()
+        path = data['path']
+        signature = data['signature']
+        generated_signature = hmac.new(settings.CMSCLOUD_SYNC_KEY, path, hashlib.sha1).hexdigest()
+        if not constant_time_compare(signature, generated_signature):
+            raise forms.ValidationError("Invalid signature")
+        return data
+
+
+class AddForm(DeleteForm):
+    content = forms.FileField(required=True)
+
+    def clean(self):
+        data = super(DeleteForm, self).clean()
+        path = data['path']
+        signature_hmac = hmac.new(settings.CMSCLOUD_SYNC_KEY, path, hashlib.sha1)
+        uploaded_file = data['content']
+        for chunk in uploaded_file.chunks():
+            signature_hmac.update(chunk)
+        generated_signature = signature_hmac.hexdigest()
+        signature = data['signature']
+        if not constant_time_compare(signature, generated_signature):
+            raise forms.ValidationError("Invalid signature")
+        return data
+
--- cmscloud/views.py
+++ cmscloud/views.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from cms.models.pluginmodel import CMSPlugin
 from cms.models.titlemodels import Title
-from django.http import HttpResponse
+from collections import defaultdict
+from django.conf import settings
+import json
+from cmscloud.forms import AddForm, DeleteForm
+from django.forms.forms import NON_FIELD_ERRORS
+from django.http import HttpResponse, HttpResponseBadRequest, Http404
+from django.views.generic import View
+import os
+
 
 def check_plugins(request):
     plugins = request.GET.get('plugins', '').split(',')
@@ -12,3 +20,42 @@ def check_apphooks(request):
     apphooks = request.GET.get('apphooks', '').split(',')
     count = Title.objects.filter(application_urls__in=apphooks).count()
     return HttpResponse(str(count))
+
+def errors_to_json(form):
+    output = defaultdict(list)
+    for field, errors in form.errors.items():
+        for error in errors:
+            output[form[field].label if field != NON_FIELD_ERRORS else NON_FIELD_ERRORS].append(error)
+    return json.dumps(output)
+
+
+class Add(View):
+    form = AddForm
+
+    def post(self, request):
+        if not getattr(settings, 'CMSCLOUD_SYNC_KEY'):
+            raise Http404()
+        form = self.form(request.POST, request.FILES)
+        if form.is_valid():
+            self.save(form.clean())
+            return HttpResponse('', status=204)
+        else:
+            return HttpResponseBadRequest(errors_to_json(form), content_type='application/json')
+
+    def save(self, data):
+        full_path = os.path.join(settings.PROJECT_DIR, data['path'])
+        directory = os.path.dirname(full_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(full_path, 'w') as fobj:
+            for chunk in data['content'].chunks():
+                fobj.write(chunk)
+
+
+class Delete(Add):
+    form = DeleteForm
+
+    def save(self, data):
+        full_path = os.path.join(settings.PROJECT_DIR, data['path'])
+        if os.path.exists(full_path):
+            os.remove(full_path)
