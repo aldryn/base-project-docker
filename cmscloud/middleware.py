2d232ba425276fcb36f2fad4c2b9c2a4d9028d12
Jonas Obrist <ojiidotch@gmail.com>
2013-06-30 23:03:36 +0900

    exclude the api from login only

--- cmscloud/middleware.py
+++ cmscloud/middleware.py
@@ -42,7 +42,7 @@ class ToolbarMiddleware(object):
 
 class AccessControlMiddleware(object):
     def process_request(self, request):
-        if not request.user.is_authenticated() and not request.path.startswith('/login/'):
+        if not request.user.is_authenticated() and not request.path.startswith(('/login/', '/admin/~cmscloud-api/')):
             view = LoginView.as_view(client=Client.from_dsn(settings.SSO_DSN))
             return view(request)
         return None
