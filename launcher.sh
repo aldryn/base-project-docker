c45c9bf02c4008838c707f21a10a686dbcb9c9fb
Jonas Obrist <ojiidotch@gmail.com>
2013-08-07 17:40:03 +0900

    added more echos

--- launcher.sh
+++ launcher.sh
@@ -4,4 +4,6 @@ python manage.py smart_database &
 DB_PID=$!
 echo "running gunicorn"
 python manage.py run_gunicorn -b 0.0.0.0:$PORT
+echo "gunicorn stopped, waiting for smart_database to stop"
 wait $DB_PID
+echo "everything stopped"
