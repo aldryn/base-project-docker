f68d7a882cb81d6ee8045f65ec8f292279ad3050
Jonas Obrist <ojiidotch@gmail.com>
2013-08-07 20:52:29 +0900

    show a intermediate login screen

--- cmscloud/middleware.py
+++ cmscloud/middleware.py
@@ -3,12 +3,24 @@
 Access Control Middleware
 """
 from django.conf import settings
+from django.http import HttpResponse
 from simple_sso.sso_client.client import LoginView, Client
 
 
 class AccessControlMiddleware(object):
     def process_request(self, request):
         if not request.user.is_authenticated() and not request.path.startswith(('/login/', '/admin/~cmscloud-api/')):
-            view = LoginView.as_view(client=Client.from_dsn(settings.SSO_DSN))
-            return view(request)
+            return HttpResponse(
+                '''
+                <html>
+                    <head>
+                        <title>Login Required</title>
+                    </head>
+                    <body>
+                        <h1>You need to log in to access this website</h1>
+                        <p>Click <a href="/login/">here</a> to log in.</p>
+                    </body>
+                </html>
+                '''
+            )
         return None
