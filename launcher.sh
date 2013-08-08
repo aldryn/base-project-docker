e1922683264f0ca617a801be9d52327a821535a2
Jonas Obrist <ojiidotch@gmail.com>
2013-08-09 01:26:24 +0900

    updated laucnher, smart_database now runs synchronously

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "running smart_database in the background"
-python manage.py smart_database &
-DB_PID=$!
+python manage.py smart_database
 echo "running gunicorn"
 python manage.py run_gunicorn -b 0.0.0.0:$PORT
-echo "gunicorn stopped, waiting for smart_database to stop"
-wait $DB_PID
-echo "everything stopped"
+echo "gunicorn stopped"
