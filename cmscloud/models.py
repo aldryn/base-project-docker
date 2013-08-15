7fdfaac039b0f6e101710701033d3287f501721b
Jonas Obrist <ojiidotch@gmail.com>
2013-08-16 02:24:30 +0900

    disconnect the signal

--- cmscloud/models.py
+++ cmscloud/models.py
@@ -5,8 +5,11 @@ from django.db.models.signals import post_save, pre_save, post_delete
 from django.conf import settings
 import requests
 
+DISPATCH_UID = 'aldryn-cms-cloud-apphook'
+
 
 def trigger_restart(**kwargs):
+    request_finished.disconnect(trigger_restart, dispatch_uid=DISPATCH_UID)
     restarter_url = getattr(settings, 'RESTARTER_URL', None)
     if not restarter_url:
         return
@@ -30,18 +33,18 @@ def apphook_post_checker(instance, **kwargs):
     """
     old_apps, old_paths = getattr(instance, '_old_data', (None, None))
     if old_apps != instance.application_urls:
-        request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
+        request_finished.connect(trigger_restart, dispatch_uid=DISPATCH_UID)
     else:
         paths = sorted(instance.title_set.values_list('path', flat=True))
         if old_paths != paths and instance.application_urls:
-            request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
+            request_finished.connect(trigger_restart, dispatch_uid=DISPATCH_UID)
 
 def apphook_post_delete_checker(instance, **kwargs):
     """
     Check if this was an apphook
     """
     if instance.application_urls:
-        request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
+        request_finished.connect(trigger_restart, dispatch_uid=DISPATCH_UID)
 
 pre_save.connect(apphook_pre_checker, sender=Page)
 post_save.connect(apphook_post_checker, sender=Page)
