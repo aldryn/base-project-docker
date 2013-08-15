48ae2165fcbf04294c743282a17673fa58ba4c71
Jonas Obrist <ojiidotch@gmail.com>
2013-08-16 02:22:18 +0900

    re-implement restarter api

--- cmscloud/models.py
+++ cmscloud/models.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from cms.models import Page
+from django.core.signals import request_finished
 from django.db.models.signals import post_save, pre_save, post_delete
+from django.conf import settings
+import requests
 
 
-def trigger_restart():
-    return
-    #restarter = getattr(settings, 'RESTARTER_URL', None)
-    #if not restarter:
-    #    return
-    #params = [('command', 'restart'), ('key', settings.SIMPLE_SSO_KEY)]
-    #signature = build_signature(params, settings.SIMPLE_SSO_SECRET)
-    #params.append(('signature', signature))
-    #requests.post(restarter, dict(params))
+def trigger_restart(**kwargs):
+    restarter_url = getattr(settings, 'RESTARTER_URL', None)
+    if not restarter_url:
+        return
+    requests.post(restarter_url, data={'info': getattr(settings, 'RESTARTER_PAYLOAD', None)})
 
 def apphook_pre_checker(instance, **kwargs):
     """
@@ -31,18 +30,18 @@ def apphook_post_checker(instance, **kwargs):
     """
     old_apps, old_paths = getattr(instance, '_old_data', (None, None))
     if old_apps != instance.application_urls:
-        trigger_restart()
+        request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
     else:
         paths = sorted(instance.title_set.values_list('path', flat=True))
         if old_paths != paths and instance.application_urls:
-            trigger_restart()
+            request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
 
 def apphook_post_delete_checker(instance, **kwargs):
     """
     Check if this was an apphook
     """
     if instance.application_urls:
-        trigger_restart()
+        request_finished.connect(trigger_restart, dispatch_uid='aldryn-cms-cloud-apphook')
 
 pre_save.connect(apphook_pre_checker, sender=Page)
 post_save.connect(apphook_post_checker, sender=Page)
