8dfa9c26c658bac371b6b4f4737ff906b7afd5d7
Jonas Obrist <ojiidotch@gmail.com>
2013-09-19 17:15:17 +0900

    added i18n urls

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, url, include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from simple_sso.sso_client.client import Client
 
@@ -14,7 +15,8 @@ client = Client.from_dsn(settings.SSO_DSN)
 urlpatterns = patterns('',
     url(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), 'django.contrib.staticfiles.views.serve', {'insecure': True}),
     url(r'^admin/~cmscloud-api/', include('cmscloud.urls')),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', include(client.get_urls())),
+) + i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
