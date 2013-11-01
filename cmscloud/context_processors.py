b55c3b34ede3352540373595e62560454016f760
Patrick Lauber <digi@treepy.com>
2013-11-01 17:42:45 +0100

    delete

--- cmscloud/context_processors.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-
-def google_analytics(request):
-    return {'GOOGLE_ANALYTICS_KEY': getattr(settings, 'GOOGLE_ANALYTICS_KEY', None)}
-
-def boilerplate(request):
-    return {'BOILERPLATE_CONFIG': getattr(settings, 'BOILERPLATE_CONFIG', {})}
