dff3b6da4dd671f2cded7de22b9463b8c47abc2d
Stefan Foulis <stefan@foulis.ch>
2014-02-07 12:22:23 +0100

    removes db-migrations on process start

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "running smart_database in the background"
-python manage.py smart_database
+#echo "running smart_database in the background"
+#python manage.py smart_database
+echo "NOT running database migrations, because they should have already been run while building"
 echo "running gunicorn"
 python manage.py run_gunicorn -b 0.0.0.0:$PORT -w 2 --max-requests 1000 --graceful-timeout 120
 echo "gunicorn stopped"
