f165f8c9448d16c6ce33013cfcd65e7ec43a60ef
Stefan Foulis <stefan@foulis.ch>
2014-02-10 11:16:14 +0100

    sets higher default thumbnail quality and retina version

--- settings.py
+++ settings.py
@@ -24,6 +24,9 @@ if 'DATABASES' not in locals():
 
 
 # TODO: remove django-filer stuff from here. It should be an addon.
+THUMBNAIL_QUALITY = 95
+THUMBNAIL_HIGH_RESOLUTION = True
+THUMBNAIL_PRESERVE_EXTENSIONS = ['png', 'gif']
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
     'easy_thumbnails.processors.autocrop',
