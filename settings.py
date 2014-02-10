ce21e28fa661752ccf9e20289f2b2fe0f298cf85
Stefan Foulis <stefan@foulis.ch>
2014-02-10 15:12:17 +0100

    upps thumnail quality again

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ if 'DATABASES' not in locals():
 
 
 # TODO: remove django-filer stuff from here. It should be an addon.
-THUMBNAIL_QUALITY = 85
+THUMBNAIL_QUALITY = 95
 # THUMBNAIL_HIGH_RESOLUTION = False  # causing timeouts?
 THUMBNAIL_PRESERVE_EXTENSIONS = ['png', 'gif']
 THUMBNAIL_PROCESSORS = (
