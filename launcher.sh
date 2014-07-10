c892a5973984f13ef59576baee60a16615d79447
Stefan Foulis <stefan@foulis.ch>
2014-07-10 10:24:49 +0200

    enables gunicorn logging to console

--- launcher.sh
+++ launcher.sh
@@ -4,5 +4,5 @@
 echo "NOT running database migrations, because they should have already been run while building"
 echo "running gunicorn"
 # python manage.py run_gunicorn -b 0.0.0.0:$PORT -w 2 --max-requests 1000 --graceful-timeout 120
-gunicorn wsgi:application -b 0.0.0.0:$PORT  -w 2 --max-requests 1000 --graceful-timeout 120 --preload --worker-class gevent --config gunicorncfg.py
+gunicorn wsgi:application -b 0.0.0.0:$PORT  -w 2 --max-requests 1000 --graceful-timeout 120 --preload --worker-class gevent --config gunicorncfg.py --access-logfile - --error-logfile - --log-level info
 echo "gunicorn stopped"
