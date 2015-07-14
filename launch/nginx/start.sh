5c4bdc053ff040d38ebf57d56f4bcfaab8002d6b
Stefan Foulis <stefan@foulis.ch>
2015-07-15 00:53:04 +0200

    allow adding extra processes to run in Procfile.extra

--- launch/nginx/start.sh
+++ launch/nginx/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+SCRIPT=$(readlink -f "$0")
+BASEDIR=$(dirname "$SCRIPT")
 export GUNICORN_PORT=5000
-BASEDIR=/app/launch/nginx_pagespeed
 cp ${BASEDIR}/nginx.conf /etc/nginx/nginx.conf
 /bin/sed -i "s/DOMAIN/${DOMAIN}/" /etc/nginx/nginx.conf
 exec forego start -f ${BASEDIR}/Procfile
