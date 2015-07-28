a4afc5fcb186fd9dffb0b8b9cb563cc55889cf88
Joseph Melettukunnel <jmelett@gmail.com>
2015-07-28 21:32:57 +0200

    Cleanup

--- cmscloud/forms.py
+++ cmscloud/forms.py
@@ -14,7 +14,8 @@ class DeleteForm(forms.Form):
         data = super(DeleteForm, self).clean()
         path = data.get('path')
         signature = data.get('signature')
-        generated_signature = hmac.new(str(settings.CMSCLOUD_SYNC_KEY), path, hashlib.sha1).hexdigest()
+        generated_signature = hmac.new(
+            str(settings.CMSCLOUD_SYNC_KEY), path, hashlib.sha1).hexdigest()
         if not constant_time_compare(signature, generated_signature):
             raise forms.ValidationError("Invalid signature")
         return data
@@ -29,7 +30,8 @@ class AddForm(DeleteForm):
         uploaded_file = data.get('content')
         signature = data.get('signature')
         if path and uploaded_file and signature:  # otherwise there were some errors
-            signature_hmac = hmac.new(str(settings.CMSCLOUD_SYNC_KEY), path, hashlib.sha1)
+            signature_hmac = hmac.new(
+                str(settings.CMSCLOUD_SYNC_KEY), path, hashlib.sha1)
             for chunk in uploaded_file.chunks():
                 signature_hmac.update(chunk)
             generated_signature = signature_hmac.hexdigest()
@@ -46,7 +48,8 @@ class RunCommandForm(forms.Form):
         data = super(RunCommandForm, self).clean()
         command = data.get('command')
         signature = data.get('signature')
-        generated_signature = hmac.new(str(settings.CMSCLOUD_SYNC_KEY), command, hashlib.sha1).hexdigest()
+        generated_signature = hmac.new(
+            str(settings.CMSCLOUD_SYNC_KEY), command, hashlib.sha1).hexdigest()
         if not constant_time_compare(signature, generated_signature):
             raise forms.ValidationError("Invalid signature")
         return data
