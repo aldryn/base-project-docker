7359858c25b5cbc64d6a59cf9c2198ab221aa782
Stefan Foulis <stefan@foulis.ch>
2015-10-12 14:20:13 +0200

    upgrade node to 0.12.7

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV NPS_VERSION=1.9.32.3\
     NGINX_VERSION=1.6.3\
     NGINX_CONF_PATH=/etc/nginx/nginx.conf\
     NGINX_PROCFILE_PATH=/etc/nginx/nginx.procfile\
-    NODE_VERSION=0.10.40\
+    NODE_VERSION=0.12.7\
     NPM_VERSION=2.13.3
 RUN /build/prepare
 
--- build/node.sh
+++ build/node.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# based on https://github.com/joyent/docker-node/blob/787d816f90dc1bf933b6503b876bdeb752b24f96/0.10/Dockerfile
+# based on https://github.com/nodejs/docker-node/blob/787d816f90dc1bf933b6503b876bdeb752b24f96/0.12/Dockerfile
 
+# verify gpg and sha256: http://nodejs.org/dist/v0.10.30/SHASUMS256.txt.asc
+# gpg: aka "Timothy J Fontaine (Work) <tj.fontaine@joyent.com>"
+# gpg: aka "Julien Gilli <jgilli@fastmail.fm>"
 set -ex \
 	&& for key in \
 		7937DFD2AB06298B2293C3187D33FF9D0246406D \
@@ -9,7 +12,7 @@ set -ex \
 		gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
 	done
 
-#ENV NODE_VERSION 0.10.40
+#ENV NODE_VERSION 0.12.7
 #ENV NPM_VERSION 2.13.3
 
 curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" \
@@ -20,3 +23,4 @@ curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.
 	&& rm "node-v$NODE_VERSION-linux-x64.tar.gz" SHASUMS256.txt.asc \
 	&& npm install -g npm@"$NPM_VERSION" \
 	&& npm cache clear
+
